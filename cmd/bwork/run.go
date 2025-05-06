fe9f58bfa63c4413f0d24ac2338ad8368c392b4b
alvaro villalobos <alvaro.villalobos@social-it.cl>
2025-05-05 22:51:57 -0400

    po

--- cmd/bwork/run.go
+++ cmd/bwork/run.go
@@ -7,12 +7,15 @@ import (
 )
 
 func runServer() {
-	fmt.Println("🚀 Ejecutando servidor desde app/main.go...")
-	cmd := exec.Command("go", "run", "./app")
+	moduleName := getModuleName()
+	fmt.Printf("🚀 Ejecutando servidor desde %s/main.go...\n", moduleName)
+
+	cmd := exec.Command("go", "run", "./"+moduleName)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error al ejecutar el servidor:", err)
+		fmt.Println("❌ Error al ejecutar el servidor:", err)
 	}
 }
